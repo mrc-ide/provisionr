26b947b936475e40c4d7da3333ed90707e967890
Rich FitzJohn <rich.fitzjohn@gmail.com>
2016-11-23 14:52:51 +0000

    Test cross installed provisioning

--- tests/testthat/test-provision-library.R
+++ tests/testthat/test-provision-library.R
@@ -16,8 +16,28 @@ test_that("own platform, single package, compiled", {
 
 test_that("own platform, multiple packages", {
   lib <- tempfile()
-  ## OK, this shows an issue; we want the library to be *standalone*
   res <- provision_library("progress", lib)
+  expect_true("progress" %in% dir(lib))
+  expect_gt(length(dir(lib)), 1)
+})
+
+test_that("cross, single package, not compiled", {
+  lib <- tempfile()
+  res <- provision_library("R6", lib, platform = "windows")
+  expect_equal(dir(lib), "R6")
+  expect_equal(.packages(TRUE, lib), "R6")
+})
+
+test_that("cross, single package, compiled", {
+  lib <- tempfile()
+  res <- provision_library("ape", lib, platform = "windows")
+  expect_equal(dir(lib), "ape")
+  expect_equal(.packages(TRUE, lib), "ape")
+})
+
+test_that("cross, multiple packages", {
+  lib <- tempfile()
+  res <- provision_library("progress", lib, platform = "windows")
   expect_true("progress" %in% dir(lib))
   expect_gt(length(dir(lib)), 1)
 })
