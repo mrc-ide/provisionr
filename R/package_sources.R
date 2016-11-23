cf2a4717232f2d65b2537f0a701fbda233c5688d
Rich FitzJohn <rich.fitzjohn@gmail.com>
2016-11-23 16:15:49 +0000

    Nice print methods

--- R/package_sources.R
+++ R/package_sources.R
@@ -55,3 +55,50 @@ local_drat <- function(src, path) {
   class(self) <- "local_drat"
   self
 }
+
+##' @export
+print.package_sources <- function(x, ...) {
+  cat(paste(as.character(x), collapse = "\n"), "\n")
+  invisible(x)
+}
+
+##' @export
+print.local_drat <- function(x, ...) {
+  cat(paste(as.character(x), collapse = "\n"), "\n")
+  invisible(x)
+}
+
+##' @export
+as.character.package_sources <- function(x, ...) {
+  f <- function(nm, list) {
+    c(sprintf("  %s:", nm),
+      sprintf("    - %s", list[[nm]]))
+  }
+  str <- c("<package_sources>",
+           sprintf("  cran: %s", x$cran),
+           if (length(x$repos) > 0L) f("repos", x))
+  if (length(x$spec) > 0L) {
+    x_spec <- split_spec(x$spec)
+    x_spec <- split(x_spec[, "value"], x_spec[, "type"])
+    str <- c(str,
+             unlist(lapply(names(x_spec), f, x_spec), use.names = FALSE))
+  }
+  str
+}
+
+##' @export
+as.character.local_drat <- function(x, ...) {
+  k <- x$db$list()
+  v <- lapply(k, x$db$get)
+  dat <- data.frame(name = vcapply(v, "[[", "Package"),
+                    version = vcapply(v, "[[", "Version"),
+                    updated = vcapply(v, function(el)
+                      prettyunits::time_ago(el$timestamp)),
+                    stringsAsFactors = FALSE)
+  str <- c("<local_drat>",
+           "  source:",
+           paste("    ", as.character(x$src)),
+           "  contents:",
+           paste("    ", capture.output(print(dat, row.names = FALSE))))
+  str
+}
