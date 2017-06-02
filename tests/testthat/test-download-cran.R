a8c578e65140cd3c1ec2dc1c428d1c1c9284c4c6
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-06-02 14:10:36 +0100

    Add tests for pulling CRAN

---
+++ tests/testthat/test-download-cran.R
@@ -0,0 +1,23 @@
+context("download-cran")
+
+test_that("download source", {
+  src <- package_sources(cran = "https://cran.rstudio.com")
+  path <- tempfile()
+  download_cran("devtools", path, "3.4.0", "ALL", FALSE, src)
+
+  db_win <- package_database(file_url(path), "windows", "3.4.0")
+  db_mac <- package_database(file_url(path), "macosx/el-capitan", "3.4.0")
+  db_src <- package_database(file_url(path), NULL, "3.4.0")
+
+  expect_true("curl" %in% rownames(db_win$all))
+  expect_true("curl" %in% rownames(db_mac$all))
+  expect_true("curl" %in% rownames(db_src$all))
+
+  path2 <- tempfile()
+  src2 <- package_sources(cran = file_url(path))
+  download_cran("devtools", path2, "3.4.0", "windows", FALSE, src2)
+  db2_win <- package_database(file_url(path2), "windows", "3.4.0")
+  db2_src <- package_database(file_url(path2), NULL, "3.4.0")
+  expect_equal(dimnames(db2_win$all), dimnames(db_win$all))
+  expect_equal(dimnames(db2_src$all), dimnames(db_src$all))
+})
