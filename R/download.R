e4899428830939e035b201699b30ac1d55ca65e6
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-06-09 15:54:02 +0100

    New download functions based on curl
    This will allow *much* nicer progress bars when downloading multiple
    files at once

---
+++ R/download.R
@@ -0,0 +1,133 @@
+progress_multi <- function(i, labels, count) {
+  label <- format(labels[[i]], width = max(nchar(labels)), justify = "right")
+  if (count) {
+    is <- format(i, width = nchar(length(labels)))
+    prefix <- sprintf("[%s/%s] %s", is, length(labels), label)
+  } else {
+    prefix <- label
+  }
+  bar <- NULL
+  type <- "down"
+  seen <- 0
+
+  progress <- function(down, up) {
+    if (type == "down") {
+      total <- down[[1L]]
+      now <- down[[2L]]
+    } else {
+      total <- up[[1L]]
+      now <- up[[2L]]
+    }
+
+    if (total == 0 && now == 0) {
+      bar <<- NULL
+      seen <<- 0
+      return(TRUE)
+    }
+
+    if (is.null(bar)) {
+      if (total == 0) {
+        fmt <- paste0(prefix, " [ :bytes in :elapsed ]")
+        total <- 1e-8 # arbitrarily big
+      } else {
+        fmt <- paste0(prefix, " [:percent :bar]")
+      }
+      bar <<- progress::progress_bar$new(fmt, total, clear = TRUE,
+                                         show_after = 0)
+    }
+    if (total == 0) {
+      bar$tick(now)
+    } else {
+      bar$tick(now - seen)
+      seen <<- now
+    }
+
+    TRUE
+  }
+  list(progress = progress,
+       prefix = prefix)
+}
+
+download_files <- function(urls, dest_dir, ..., labels = NULL,
+                           overwrite = FALSE, count = TRUE,
+                           dest_file = NULL, copy_file_urls = TRUE,
+                           report = TRUE) {
+  if (!is_directory(dest_dir)) {
+    stop("dest_dir must be a directory")
+  }
+  if (is.null(labels)) {
+    labels <- substr(basename(urls), 1, ceiling(getOption("width", 80) / 4))
+  } else {
+    if (length(labels) != length(urls)) {
+      stop("length(labels) must be the same as length(urls)")
+    }
+  }
+  if (is.null(dest_file)) {
+    dest_file <- basename(urls)
+  } else {
+    if (length(dest_file) != length(urls)) {
+      stop("length(dest_file) must be the same as length(urls)")
+    }
+  }
+
+  filename <- file.path(dest_dir, dest_file)
+  dest_file <- format(dest_file)
+
+  for (i in seq_along(urls)) {
+    f <- filename[[i]]
+    if (file.exists(f) && !overwrite) {
+      next
+    }
+
+    u <- urls[[i]]
+    p <- progress_multi(i, labels, count)
+    dir.create(dirname(f), FALSE, TRUE)
+    if (is_file_url(u)) {
+      u <- file_unurl(u)
+      if (copy_file_urls) {
+        file.copy(u, filename[[i]], overwrite = overwrite)
+      } else {
+        filename[[i]] <- u
+        next
+      }
+    } else {
+      h <- curl::new_handle(noprogress = FALSE,
+                            progressfunction = p$progress, ...)
+      f_dl <- paste0(f, "_dl")
+      if (file.exists(f_dl)) {
+        stop("Remove stale download file: ", f_dl)
+      }
+      res <- withCallingHandlers(curl::curl_fetch_disk(u, f_dl, h),
+                                 error = function(e) file.remove(f_dl))
+      if (res$status_code > 300) {
+        file.remove(f_dl)
+        stop(download_error(res))
+      }
+      file.rename(f_dl, f)
+    }
+    s <- format(structure(file.size(f), class = "object_size"),
+                units = "auto")
+    if (report) {
+      message(sprintf("%s => %s (%s)", p$prefix, dest_file[[i]], s))
+    }
+  }
+
+  filename
+}
+
+## So what does download 1 file look like?
+download_file1 <- function(url, dest_dir, ..., label = NULL,
+                           overwrite = FALSE,
+                           dest_file = NULL, copy_file_url = TRUE,
+                           report = TRUE) {
+  download_files(url, dest_dir, labels = label,
+                 overwrite = overwrite, dest_file = dest_file,
+                 copy_file_urls = copy_file_url, report = report,
+                 count = FALSE)
+}
+
+download_error <- function(r) {
+  msg <- sprintf("Downloading '%s' failed with code %d", r$url, r$status_code)
+  structure(list(message = msg, r = r, call = NULL),
+            class = c("download_error", "error", "condition"))
+}
